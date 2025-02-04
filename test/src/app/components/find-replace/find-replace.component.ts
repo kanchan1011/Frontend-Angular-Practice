85bcc50176dc47ae7797e29f5ae4a4a13ebf3358
Kanchan Gaikawad <kanchangaikawad@gmail.com>
2025-02-04 18:13:33 +0530

    added RouterModule to ts file

--- test/src/app/components/find-replace/find-replace.component.ts
+++ test/src/app/components/find-replace/find-replace.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-find-replace',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule,RouterModule],
   templateUrl: './find-replace.component.html',
   styleUrl: './find-replace.component.css',
 })

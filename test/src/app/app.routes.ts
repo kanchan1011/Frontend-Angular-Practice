9bdb79e31d8e4251377408e58a111ff22038dd44
Kanchan Gaikawad <kanchangaikawad@gmail.com>
2025-02-04 13:21:16 +0530

    Created Home, Find-Replace , Page not found components and added features of find replace

--- test/src/app/app.routes.ts
+++ test/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { FindReplaceComponent } from './components/find-replace/find-replace.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, title: 'Home Page' },
+  {
+    path: 'find-replace',
+    component: FindReplaceComponent,
+    title: 'Find & Replace Page',
+  },
+  { path: '**', component: PageNotFoundComponent },
+];
---
+++ test/src/app/components/find-replace/find-replace.component.ts
@@ -0,0 +1,30 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-find-replace',
+  standalone: true,
+  imports: [FormsModule,CommonModule],
+  templateUrl: './find-replace.component.html',
+  styleUrl: './find-replace.component.css'
+})
+export class FindReplaceComponent {
+
+  
+  inputText: string = '';
+ 
+  findText: string = '';
+  replaceText: string = '';
+  
+  resultText: string = '';
+
+  findAndReplace() {
+    if (this.findText && this.replaceText) {
+      const regex = new RegExp(this.findText, 'g');
+      this.resultText = this.inputText.replace(regex, this.replaceText);
+    } else {
+      alert('Please provide both find and replace Text.');
+    }
+  }
+}

9efdc4b93028029c18c8f651d4f29ced72329dff
Kanchan Gaikawad <kanchangaikawad@gmail.com>
2025-02-07 16:01:44 +0530

    First commit for Interview Practice app

---
+++ InterviewPractice/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent, title: 'Home Page' },
+  { path: 'about', component: AboutComponent ,title:'About Page'},
+  { path: '**', component: PageNotFoundComponent, title: 'Page Not Found' },
+];

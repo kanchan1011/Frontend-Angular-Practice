8b8c8c895c697f3ea9d2615c60a641096e63ca75
Kanchan Gaikawad <kanchangaikawad@gmail.com>
2025-05-28 06:28:36 +0530

    created userlist and home component

--- mocktest/src/app/app.routes.ts
+++ mocktest/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { UserListComponent } from './components/user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent, title:'Home Page'},
+    {path:'', pathMatch:'full',redirectTo:'home'},
+    {path:'user-list',component:UserListComponent,title:'User List'}
+
+];
